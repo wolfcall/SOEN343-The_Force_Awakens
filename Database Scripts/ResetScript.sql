6b4492dbeb3d7385a6777ad70482cfed97871d59
wolfcall <gmathieu6@gmail.com>
2016-09-24 22:23:39 -0400

    Databse script base inserts and dbconnection

--- Database Scripts/ResetScript.sql
+++ Database Scripts/ResetScript.sql
@@ -12,3 +12,11 @@ CREATE TABLE IF NOT EXISTS `soen343`.`Student` (
   `email` VARCHAR(70) UNIQUE NOT NULL,
   PRIMARY KEY (`idStudent`))
 ENGINE = InnoDB;
+
+Insert into `soen343`.`Student` (FirstName, LastName, `password`, email) values ('Georges','Mathieu',password('pass123'),'gm@email.com');
+Insert into `soen343`.`Student` (FirstName, LastName, `password`, email) values ('Stephano','Pace',password('pass123'),'sp@email.com');
+Insert into `soen343`.`Student` (FirstName, LastName, `password`, email) values ('Olivier','C',password('pass123'),'oc@email.com');
+Insert into `soen343`.`Student` (FirstName, LastName, `password`, email) values ('Nicholas','Burdet',password('pass123'),'nb@email.com');
+Insert into `soen343`.`Student` (FirstName, LastName, `password`, email) values ('Joey','T',password('pass123'),'jt@email.com');
+Insert into `soen343`.`Student` (FirstName, LastName, `password`, email) values ('Julien','P',password('pass123'),'jp@email.com');
+Insert into `soen343`.`Student` (FirstName, LastName, `password`, email) values ('Adam','Acaro',password('pass123'),'aa@email.com');
---
+++ PHP/Utilities/CommonFunctions.php
@@ -0,0 +1,32 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function getCon(){
+	$servernamelocal = "192.168.2.36";
+	$servernameremote = "wolfcall.ddns.net";
+	$port = 3306;
+	$username = "SOEN341user";
+	$password = "G3tR3ck3dS0n";
+	$schema = "soen341";
+	
+	$conn = new mysqli($servernameremote, $username, $password, $schema, $port);
+	
+	if($conn->connect_error){
+		$conn  = new mysqli($servernamelocal, $username, $password, $schema, $port);
+		
+		if($conn->connect_error)
+			die("Connection failed: " . $conn->connect_error);
+	}
+	return $conn;
+}
+//closes a connection takes a connection object
+function closeCon($conn){
+	$conn->close();
+}
+
+?>
