e2072eace174af1397e8967a2aa167462daf3aea
Adam Arcaro <adamarcaro@gmail.com>
2016-10-04 13:02:02 -0400

    Small Changes
    Redirection changes for Georges

--- Site/Index.php
+++ Site/Index.php
@@ -74,7 +74,7 @@
 </div>
 
   <!-- Modal -->
-  <form action="../PHP/SOEN343/runtime.php" method="post">
+  <form action="../PHP/pages/runtime.php" method="post">
   <div class="modal fade" id="myModal" role="dialog">
     <div class="modal-dialog">
 <!-- Modal content-->
