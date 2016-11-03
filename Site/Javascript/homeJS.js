1474027392e0ab99cb278f1c4853c617e88b4f2f
PopMyStack <julian.ippolito@hotmail.com>
2016-11-03 16:26:37 -0400

    Seperated the Javascript from Home.php
    Removed the Javascript from Home.php and put it in a seperate file
    called homeJS.js inside the javascript folder for cleanliness.

---
+++ Site/Javascript/homeJS.js
@@ -0,0 +1,55 @@
+//The Javascript file containing the javascript from PHP/Pages/Home.php.
+$(document).ready(function() {
+			var monthNames = ["January", "February", "March", "April", "May", "June",
+				"July", "August", "September", "October", "November", "December"];
+			var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+			var currentDate = new Date();
+			
+			todayDate = dayNames[currentDate.getDay()] + ", " 
+								+ monthNames[currentDate.getMonth()] + " " 
+								+ currentDate.getDate() + " "
+								+ currentDate.getUTCFullYear();
+
+			document.getElementById("datetoday").innerHTML = todayDate;
+			document.getElementById("dateDrop").value = todayDate;
+			/** end of the function, resued in "onSelect" feature of datepicker */
+			
+			$("#datepickerInline").datepicker({
+        	//	buttonImage: '../img/calendar.png',
+        	//	buttonImageOnly: true,
+        		changeMonth: true,
+        		changeYear: true,
+        		showOn: 'both',
+				minDate: 0,
+				maxDate: new Date(2018, 11, 31),
+				onSelect: function(event) {
+					var pickedDate = $("#datepickerInline").datepicker("getDate");
+					todayDate = dayNames[pickedDate.getDay()] + ", " 
+								+ monthNames[pickedDate.getMonth()] + " " 
+								+ pickedDate.getDate() + " "
+								+ pickedDate.getUTCFullYear();
+					document.getElementById("datetoday").innerHTML = todayDate;
+					document.getElementById("dateDrop").value = todayDate;
+				}
+     		});
+  		});
+
+		$(document).ready(function(){
+			$("#myBtn").click(function(){
+				$("#myModal").modal();
+			});
+		});
+
+		$(document).ready(function(){
+			$(".slot").click(function(){
+				$("#myModal").modal();
+			});
+		});
+	
+		$(document).ready(function(){
+			$("#second-r").click(function(){
+				$("#profilemyModal").modal();
+			});
+		});
+	
+		
