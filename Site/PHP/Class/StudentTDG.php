d6825fbe8c12e43fbdf8e8283768acd5687e3da1
NoGoodNamesRemaining <stefano.pace12@gmail.com>
2016-11-13 20:14:29 -0500

    Fixed Password and Email changing minor Bug

--- Site/PHP/Class/StudentTDG.php
+++ Site/PHP/Class/StudentTDG.php
@@ -140,15 +140,14 @@ class StudentTDG
 		
 		if ($conn->affected_rows > 0)
 		{
-			$string = "Password Updated Successfully";
 			$_SESSION["msgClass"] = "success";
+			return true;
 		}
 		else
 		{
-			$string = "Your current Password is not the one you entered. Please try again!";
 			$_SESSION["msgClass"] = "failure";
+			return false;
 		}
-		return $string;
     }
 }
 ?>
