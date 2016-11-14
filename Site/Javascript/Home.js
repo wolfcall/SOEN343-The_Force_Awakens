16ca89142e88f2e2245110ddc3ec01fd1036c69e
wolfcall <gmathieu6@gmail.com>
2016-11-14 17:03:11 -0500

    removed alert

--- Site/Javascript/Home.js
+++ Site/Javascript/Home.js
@@ -55,7 +55,6 @@ $(document).ready(function(){
 	
 	$("#endTime").change(function(){
 		diff = $("#endTime").prop('selectedIndex') - $("#startTime").prop('selectedIndex');
-		alert(diff);
 	});
 });
 
